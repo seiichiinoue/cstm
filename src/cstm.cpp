7b246382ec62a86ea61f12cf8350aa609b450011
Seiichi Inoue <d35inou108@gmail.com>
2019-10-08 00:02:13 +0900

    added initialization

--- src/cstm.cpp
+++ src/cstm.cpp
@@ -1,1 +1,26 @@
 #include "cstm.hpp"
+
+CSTM::CSTM() {
+        _ndim_d = NDIM_D;
+        _sigma_u = SIGMA_U;
+        _sigma_phi = SIGMA_PHI;
+        _sigma_alpha0 = SIGMA_ALPHA;
+        _gamma_alpha_a = GAMMA_ALPHA_A;
+        _gamma_alpha_b = GAMMA_ALPHA_B;
+        _g0 = NULL;
+        _word_vectors = NULL;
+        _doc_vectors = NULL;
+        _n_k = NULL;
+        _word_count = NULL;
+        _sum_n_k = NULL;
+        _Zi = NULL;
+        _log_likelihood_first_term = NULL;
+        _tmp_vec = NULL;
+        _vocabrary_size = 0;
+        _num_documents = 0;
+        _sum_word_frequency = 0;
+        _ignore_word_count = 0;
+        _standard_normal_distribution = normal_distribution<double>(0, 1);
+}
+
+CSTM::~CSTM() {}
--- src/cstm.hpp
+++ src/cstm.hpp
@@ -14,6 +14,7 @@
 #include <unordered_map>
 #include <boost/random.hpp>
 #include "sampler.hpp"
+
 #define PI 3.14159265358979323846
 #define NDIM_D 2                // 文書,単語ベクトルの次元数
 #define SIGMA_U 0.01            // 文書ベクトののランダムウォーク幅
@@ -49,29 +50,7 @@ public:
     normal_distribution<double> _noise_doc;
     normal_distribution<double> _noise_alpha0;
 
-    CSTM() {
-        _ndim_d = NDIM_D;
-        _sigma_u = SIGMA_U;
-        _sigma_phi = SIGMA_PHI;
-        _sigma_alpha0 = SIGMA_ALPHA;
-        _gamma_alpha_a = GAMMA_ALPHA_A;
-        _gamma_alpha_b = GAMMA_ALPHA_B;
-        _g0 = NULL;
-        _word_vectors = NULL;
-        _doc_vectors = NULL;
-        _n_k = NULL;
-        _word_count = NULL;
-        _sum_n_k = NULL;
-        _Zi = NULL;
-        _log_likelihood_first_term = NULL;
-        _tmp_vec = NULL;
-        _vocabrary_size = 0;
-        _num_documents = 0;
-        _sum_word_frequency = 0;
-        _ignore_word_count = 0;
-        _standard_normal_distribution = normal_distribution<double>(0, 1);
-    }
-    ~CSTM() {
-    }
+    CSTM();
+    ~CSTM();
 
 };
