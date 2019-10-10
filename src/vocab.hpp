e6b631ac0bcc70bbb3fdc643f960120053e7d18c
Seiichi Inoue <d35inou108@gmail.com>
2019-10-11 02:10:16 +0900

    added vocab header

---
+++ src/vocab.hpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <fstream>
+#include <stddef.h>
+#include <assert.h>
+#include <limits>
+#include <stdlib.h>
+#include <float.h>
+#include <string.h>
+#include <boost/serialization/serialization.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/unordered_map.hpp>
+#include "fmath.hpp"
+using namespace std;
+using id = size_t;
+
+class Vocab {
+public:
+    unordered_map<id, wstring> _string_by_word_id;
+    unordered_map<id, id> _hash_to_id;
+    hash<wstring> _hash_func;
+    Vocab() {}
+    id add_string(wstring &str) {
+        id hash = hash_string(str);
+        auto itr = _hash_to_id.find(hash);
+        return itr->second;
+    }
+    id get_word_id(wstring &str) {
+        id hash = hash_string(str);
+        auto itr = _hash_to_id.find(hash);
+        return itr->second;
+    }
+    id hash_string(wstring &str) {
+        return (id)_hash_func(str);
+    }
+    wstring word_id_to_string(id word_id) {
+        auto itr = _string_by_word_id.find(word_id);
+        return itr->second;
+    }
+    wstring token_ids_to_sentence(vector<id> &token_ids) {
+        wstring sentence = L"";
+        for (const auto &word_id : token_ids) {
+            wstring word = word_id_to_string(word_id);
+            sentence += word;
+            sentence += L" ";
+        }
+        return sentence;
+    }
+    int num_words() {
+        return _string_by_word_id.size();
+    }
+};
