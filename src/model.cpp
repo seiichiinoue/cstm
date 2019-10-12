01393b6d0d16ff09579319a09b79de14cba90af8
Seiichi Inoue <d35inou108@gmail.com>
2019-10-13 01:37:35 +0900

    added model

---
+++ src/model.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <unordered_map> 
+#include "cstm.hpp"
+#include "vocab.hpp"
+
+template<typename T>
+struct multiset_comparator {
+    bool operator()(const pair<id, T> &a, const pair<id, T> &b) {
+        return a.second > b.second;
+    }
+};
+
+void split_string_by(const string &str, char delim, vector<string> &elems) {
+    elems.clear();
+    string item;
+    for (char ch : str) {
+        if (ch == delim) {
+            if (!item.empty()) {
+                elems.push_back(item);
+            }
+            item.clear();
+        } else {
+            item += ch;
+        }
+    }
+    if (!item.empty()) {
+        elems.push_back(item);
+    }
+}
+void split_word_by(const wstring &str, wchar_t delim, vector<wstring> &elems) {
+    elems.clear();
+    wstring item;
+    for (wchar_t ch : str) {
+        if (ch == delim) {
+            if (!item.empty()) {
+                elems.push_back(item);
+            }
+            item.clear();
+        } else {
+            item += ch;
+        }
+    }
+    if (!item.empty()) {
+        elems.push_back(item);
+    }
+}
+
+class CSTMTrainer {
+
+};
