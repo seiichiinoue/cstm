02f5c15cb56f243830ff60941c3399331177358d
Seiichi Inoue <d35inou108@gmail.com>
2019-10-14 00:05:47 +0900

    defined namespace

--- src/common.hpp
+++ src/common.hpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include "fmath.hpp"
 
 #define NDIM_D 2                // 文書,単語ベクトルの次元数
 #define SIGMA_U 0.01            // 文書ベクトののランダムウォーク幅
@@ -10,29 +9,31 @@
 #define GAMMA_ALPHA_B 1         // a0がガンマ事前分のはハイパーパラメータ
 using id = size_t;
 
-double exp(double x) {
-    return std::exp(x);
-}
+namespace cstm {
+    double exp(double x) {
+        return std::exp(x);
+    }
 
-double norm(double *a, double *b, int length) {
-    double norm = 0;
-    for (int i=0; i<length; ++i) {
-        norm += a[i] * a[i];
+    double norm(double *a, double *b, int length) {
+        double norm = 0;
+        for (int i=0; i<length; ++i) {
+            norm += a[i] * a[i];
+        }
+        return sqrt(norm);
     }
-    return sqrt(norm);
-}
 
-double inner(double *a, double *b, int length) {
-    double inner = 0;
-    for (int i=0; i<length; ++i) {
-        inner += a[i] * b[i];
+    double inner(double *a, double *b, int length) {
+        double inner = 0;
+        for (int i=0; i<length; ++i) {
+            inner += a[i] * b[i];
+        }
+        return inner;
     }
-    return inner;
-}
-void dump_vec(double *vec, int len) {
-    std::cout << "[";
-    for (int i=0; i<len-1; ++i) {
-        std::cout << vec[i] << ", ";
+    void dump_vec(double *vec, int len) {
+        std::cout << "[";
+        for (int i=0; i<len-1; ++i) {
+            std::cout << vec[i] << ", ";
+        }
+        std::cout << vec[len-1] << "]" << std::endl;
     }
-    std::cout << vec[len-1] << "]" << std::endl;
 }
