786eea02ca6a094dc9c277c6a90521eb34c279bf
Seiichi Inoue <d35inou108@gmail.com>
2019-10-20 01:28:18 +0900

    added lang code settings for fixing garbling

--- utils/plot_word.py
+++ utils/plot_word.py
@@ -1,22 +1,25 @@
-import pylab
+# import plt
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 import argparse, sys, os
 sys.path.append(os.getcwd())
 import pycstm
+plt.rcParams['font.family'] = 'IPAGothic'
+sns.set(font=["IPAGothic"], font_scale=2)
 
 def plot_words(words, ndim_vector):
-    with sns.axes_style("white", {"font.family": ["MS Gothic"]}):
+    with sns.axes_style("white", {"font.family": ["IPAGothic"]}):
         for i in range(ndim_vector-1):
-            fig = pylab.gcf()
+            fig = plt.gcf()
             fig.set_size_inches(45.0, 45.0)
-            pylab.clf()
+            plt.clf()
             for meta in words:
                 word_id, word, count, vector = meta
-                pylab.text(vector[i], vector[i+1], word, fontsize=8)
-            pylab.xlim(-4, 4)
-            pylab.ylim(-4, 4)
-            pylab.savefig("./data/fig/"+"words_{}_{}.png".format(i, i+1))
+                plt.text(vector[i], vector[i+1], word, fontsize=8)
+            plt.xlim(-4, 4)
+            plt.ylim(-4, 4)
+            plt.savefig("./data/fig/"+"words_{}_{}.png".format(i, i+1))
 
 if __name__ == "__main__":
     cstm = pycstm.cstm("./model/cstm.model")
