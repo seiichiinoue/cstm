36b9c87d653126c2ccd966490c2854bcb04a2efd
Seiichi Inoue <d35inou108@gmail.com>
2019-10-20 02:14:41 +0900

    changed using lib

--- utils/plot_doc.py
+++ utils/plot_doc.py
@@ -1,24 +1,24 @@
-import pylab
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 import argparse, sys, os
 sys.path.append(os.getcwd())
 import pycstm
 
 # def plot_kde(data, filename):
-#     fig = pylab.gcf()
+#     fig = plt.gcf()
 #     fig.set_size_inches(16.0, 16.0)
-#     pylab.clf()
+#     plt.clf()
 #     ax = sns.kdeplot(data[:, 0], data[:, 1], shade=True, cmap="Greens", n_levels=30, clip=[[-4, 4]] * 2)
 #     kde = ax.get_figure()
 #     kde.savefig('./data/fig/'+filename+'.png')
 
 def plot_scatter(data, filename):
-    fig = pylab.gcf()
+    fig = plt.gcf()
     fig.set_size_inches(16.0, 16.0)
-    pylab.clf()
-    pylab.scatter(data[:, 0], data[:, 1], s=20, marker="o", edgecolors="none", color="blue")
-    pylab.savefig('./data/fig/'+filename+'.png')
+    plt.clf()
+    plt.scatter(data[:, 0], data[:, 1], s=20, marker="o", edgecolors="none", color="blue")
+    plt.savefig('./data/fig/'+filename+'.png')
 
 if __name__ == "__main__":
     cstm = pycstm.cstm("./model/cstm.model")
