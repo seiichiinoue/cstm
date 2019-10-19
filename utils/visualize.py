1b0e768270bce5a5f0c2f80d7382d7449bc915ea
Seiichi Inoue <d35inou108@gmail.com>
2019-10-19 12:32:11 +0900

    removed old file

--- utils/visualize.py
+++
@@ -1,9 +0,0 @@
-import argparse, sys, os
-sys.path.append(os.getcwd())
-import pycstm
-
-if __name__ == "__main__":
-    cstm = pycstm.cstm("./model/cstm.model")
-    ndim_d = cstm.get_ndim_d()
-
-    doc_vectors = cstm.get_doc_vectors()
