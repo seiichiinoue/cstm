289d94fee78d31de731d5a296a7931850e80eeba
Seiichi Inoue <d35inou108@gmail.com>
2019-10-20 21:07:29 +0900

    added func detect lang type

--- utils/process.py
+++ utils/process.py
@@ -1,5 +1,4 @@
-import argparse
-import os
+import argparse, os, unicodedata
 import MeCab
 
 class Text(object):
@@ -11,12 +10,23 @@ class Text(object):
         for i in range(len(self.text)):
             self.text[i] = self.text[i].replace("\n", "")
 
+    def _is_japanese(self, text):
+        for c in ['CJK UNIFIED', 'HIRAGANA', 'KATAKANA']:
+            if c in unicodedata.name(text):
+                return True
+        return False
+
+    def _wakati(self, path):
+        if self._is_japanese(self.text[0][0]):
+            self._wakati_ja(path)
+        else:
+            self._wakati_en(path)
+        
     def _wakati_ja(self, path):
         wakatied = []
         for i in range(len(self.text)):
             new_t = self.wakati.parse(self.text[i]).strip("\n")
             wakatied.append(new_t)
-        
         with open(path, "w") as f:
             for t in wakatied:
                 f.write(t+'\n')
@@ -34,20 +44,11 @@ class Text(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='this script for text processing.')
-    parser.add_argument('--lang_type', help='english or japanese')
     parser.add_argument('--tar_path', help='directory located raw file')
     parser.add_argument('--save_path', help='directory save processed text')
     args = parser.parse_args()
     filelist = os.listdir(args.tar_path)
-    if args.lang_type.lower() == 'japanese':
-        for file in filelist:
-            if file.endswith(".txt"):
-                t = Text(args.tar_path+file)
-                t._wakati_ja(args.save_path+file)
-    elif args.lang_type.lower() == 'english':
-        for file in filelist:
-            if file.endswith(".txt"):
-                t = Text(args.tar_path+file)
-                t._wakati_en(args.save_path+file)
-    else:
-        print("Error: this script does not support language of got file")
+    for file in filelist:
+        if file.endswith(".txt"):
+            t = Text(args.tar_path+file)
+            t._wakati(args.save_path+file)
